853045fbf7362cccc8caf636d554b33f23930679
king-jojo <kimwhu@outlook.com>
2018-05-08 19:32:22 +0800

    Update

--- compass/hook/find_code.py
+++ compass/hook/find_code.py
@@ -34,7 +34,7 @@ def find_code(keyword):
 				if begin_line <= check_point and end_line >= check_point:
 					line_num = "%08d" % (check_point) + "%08d" % (file_num)
 					line_info_dict[line_num] = line
-		file_dict[file_path[file_num]] = [line_info_dict]
+		file_dict[file_path[file_num].replace("/home/zjin","") + ":" + str(begin_line) + "-" + str(end_line)] = [line_info_dict]
 		output_list.append(file_dict)
 	return output_list
 

45256719edf113ed36c08dfca8eb38b6cd011bf4
king-jojo <kimwhu@outlook.com>
2018-06-26 13:14:12 +0800

    Update app.py

--- compass/webApp/app.py
+++ compass/webApp/app.py
@@ -1,10 +1,14 @@
 #!flask/bin/python
 from flask import Flask, render_template, jsonify, request
+from setup.datapath import home_path
 import json
+import os
 import semanticGraph, codeSection, commitHistory, prInfo
 
-nodeFilePath = "/home/user0/testDataForCompass/nodes.json"
-edgeFilePath = "/home/user0/testDataForCompass/edges.json"
+path = os.path.expandvars('$HOME')
+
+nodeFilePath = home_path + "nodes.json"
+edgeFilePath = home_path + "edges.json"
 
 app = Flask(__name__)
 graph = semanticGraph.semanticGraph(nodeFilePath, edgeFilePath)
@@ -25,23 +29,23 @@ def getLineInfo():
 	fileID = tmp['fileID'].encode('utf-8')
 	lineID = tmp['lineID']
 	result = commitHistory.get_author(fileID, lineID, 
-						'/home/user0/testDataForCompass', 
-						"/home/user0/testDataForCompass/file_path.json")
+						home_path, 
+						home_path + "file_path.json")
 	return jsonify({'developerName':result[0], 'lineNumList':result[1]}), 201
 
 @app.route("/getCodeSection", methods=['POST'])
 def getCodeSection():
 	declaration = json.loads(request.get_data())['declaration'].encode('utf-8')
 	result = codeSection.find_code(declaration, 
-					"/home/user0/testDataForCompass/line_dict_new.json", 
-					"/home/user0/testDataForCompass/file_path.json", 
-					"/home/user0")
+					home_path + "line_dict_new.json", 
+					home_path + "file_path.json", 
+					path)
 	return jsonify({'codeSection':result}), 201
 
 @app.route("/getPRInfo", methods=['POST'])
 def getPRInfo():
 	codeLine = json.loads(request.get_data())['codeLine'].encode('utf-8')
-	result = prInfo.getPRInfo(codeLine, "/home/user0/testDataForCompass/code2pr.json")
+	result = prInfo.getPRInfo(codeLine, home_path + "code2pr.json")
 	return jsonify({'prLinks':result}), 201
 
 if __name__ == '__main__':

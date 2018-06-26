7b195d4827565b0c57aecd5fdd7cbebcd926c499
Zhenghui JIn <kimwhu@outlook.com>
2018-06-26 14:00:10 +0800

    Update app.py

--- compass/webApp/app.py
+++ compass/webApp/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from flask import Flask, render_template, jsonify, request
-from setup.datapath import home_path
+from datapath import home_path
 import json
 import os
 import semanticGraph, codeSection, commitHistory, prInfo

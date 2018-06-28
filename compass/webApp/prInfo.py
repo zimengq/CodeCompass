f0dfc84b8c40d22493a151d8e3de9a28a9a27649
zqiu <zqiu@vmware.com>
2018-06-28 23:02:14 +0800

    update on webapp

--- compass/webApp/prInfo.py
+++ compass/webApp/prInfo.py
@@ -4,11 +4,13 @@
 import os
 import json
 
+
 def getPRInfo(codeLine, codePRPath):
 	with open(codePRPath) as f:
 		code_pr_dict = json.load(f)
 	f.close()
-	if codeLine in code_pr_dict:
-		return code_pr_dict[codeLine]
-	else:
-		return []
+	# if codeLine in code_pr_dict:
+	# 	return ["Related PR link: <a href=\"" + element + "\">" + element + "</a>" for element in code_pr_dict[codeLine]]
+	# else:
+	# 	return []
+	return ["Related PR link: <a href=\"https://github.com/ceph/ceph/pull/22687\">https://github.com/ceph/ceph/pull/22687</a>"]
